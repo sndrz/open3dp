769fffd98bf445f8265c6092f4695cc16c7fc0c5
Sergey Ivanov <sand.mrt@gmail.com>
2013-12-23 17:13:54 +0400

    Add 3dp-mon core.* templates

---
+++ software/3dp-mon/src/core.h
@@ -0,0 +1,16 @@
+﻿#ifndef __CORE_H
+#define __CORE_H
+
+#define	_FETCHER_BUFFER_SIZE	1024
+
+class _fetcher{
+private:
+
+public:
+	_fetcher();
+	~_fetcher();
+	
+	loadGCode(char *filename);
+}; // class _fetcher
+
+#endif
