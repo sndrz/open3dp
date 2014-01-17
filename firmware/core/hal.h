4cbd12df251fb857d4b580ef6d6f79999f86cb8c
Sergey Ivanov <sand.mrt@gmail.com>
2014-01-17 16:07:35 +0400

    Add small changes to HAL

--- firmware/core/hal.h
+++ firmware/core/hal.h
@@ -1,19 +1,24 @@
 ﻿#ifndef __HAL_H
 #define __HAL_H
 
+//#define F_CPU	100000000U
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
+//#include <avr/timer.h>
 
 /**
-*	Some macros were used from AVR-TOOLBOX/base.h
-*	They should be redefined with similar definition
-*	for other architectures (if neccessary).
+*	Hardware Asbtract Layer area for AVR-TOOLBOX library
+*
+*	All macros and functions from these includes below
+*	should be redefined with similar headers for other
+*	architectures (if neccessary).
 */
 #include "../../avr-toolbox/base.h"
 
 /** Pin configuration */
 
-/// Motor section
+/// Motors
 
 #define _pin_ddr_mo_x	0xff
 #define _pin_prt_mo_x	0xff
@@ -31,6 +36,18 @@
 #define _pin_prt_mo_h	0xff
 #define	_pin_pin_mo_h	0xff
 
+/// Hot seat
+
+#define _pin_ddr_hs_ht	0xff
+#define _pin_prt_hs_ht	0xff
+#define _pin_pin_hs_ht	0xff
+
+/// Hot end
+
+#define _pin_ddr_he_ht	0xff
+#define _pin_prt_he_ht	0xff
+#define _pin_pin_he_ht	0xff
+
 /** Functions */
 
 void init_mcu(void);
@@ -38,5 +55,6 @@ void init_mcu(void);
 /** Interrupt vectors */
 
 //ISR(USART_RXT_vect);
+//ISR(USART_TXT_vect);
 
 #endif
