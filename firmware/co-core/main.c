49f0b13438b446dfc9cded15e7eaa7047aaa1927
Sergey Ivanov <sand.mrt@gmail.com>
2014-04-02 17:24:18 +0400

    Update firmware file structure

---
+++ firmware/co-core/main.c
@@ -0,0 +1,21 @@
+/**	open3dp :: graph.c
+*
+*	A graphical co-controller firmware.
+*	Main program.
+*
+*	Sergey Ivanov <sand.mrt@gmail.com>
+*
+*	GNU General Public License
+*	http://www.gnu.org/licenses/gpl.html
+*/
+
+//#include "st7920.h"
+
+void main(void){
+
+	/// Main loop
+	while (1) {
+	
+	}
+
+} // main
